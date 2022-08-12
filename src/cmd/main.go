1fd95ddea90f49241a21c91dcb5a1c00f1f227dc
AUTH\sancheni <niels.sanchez.van.den.beuken@hp.com>
2022-08-12 22:10:07 +0000

    env vars added

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -6,15 +6,20 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load("../../env/variables.env")
+	if err != nil {
+		fmt.Println("Error loading env vars " + err.Error())
+	}
 	r := gin.Default()
 	app := r.Group("/contentsquare")
 	DBRepo := repositories.NewMySqlRepository()
 	handlers.NewIngestHandler(app, DBRepo)
 
-	err := r.Run(":8080")
+	err = r.Run(":8080")
 	if err != nil {
 		fmt.Println("Error running server " + err.Error())
 	}
